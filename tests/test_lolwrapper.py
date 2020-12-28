9ee2f957b3c290c47f81601ce483abe67486ccac
Victor Aquiles <victoraqalencar@gmail.com>
2020-12-28 19:31:44 -0300

    Finish mastery endpoints

--- tests/test_lolwrapper.py
+++ tests/test_lolwrapper.py
@@ -28,7 +28,7 @@ def test_wrong_region():
     assert ', '.join(list(REGION_URL.keys())) in str(region_info.value)
 
 
-def test_summoner_champion_mastery(environment):
+def test_summoner_champion_mastery_list(environment):
     """Tests an API call to get the all champion mastery`s of a summoner"""
 
     wrapper = LoLWrapper(environment['api_key'], region="BR1")
@@ -39,3 +39,27 @@ def test_summoner_champion_mastery(environment):
     assert isinstance(response, list)
     assert isinstance(response[0], dict)
     assert response[0]["summonerId"] == account_id
+
+def test_summoner_champion_mastery(environment):
+    """Tests an API call to get a champion mastery of a summoner"""
+
+    wrapper = LoLWrapper(environment['api_key'], region="BR1")
+
+    account_id = environment['account_id']
+    champion_id = 43
+    response = wrapper.summoner_champion_mastery(account_id, champion_id)
+
+    assert isinstance(response, dict)
+    assert response["championId"] == champion_id
+    assert response["summonerId"] == account_id
+
+def test_summoner_mastery_score(environment):
+    """Tests an API call to get a mastery score of a summoner"""
+
+    wrapper = LoLWrapper(environment['api_key'], region="BR1")
+
+    account_id = environment['account_id']
+
+    response = wrapper.summoner_mastery_score(account_id)
+
+    assert isinstance(response, int)
