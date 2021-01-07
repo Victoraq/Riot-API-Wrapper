e4668d4a908be7095d55a9a4be5182a5fa8674ee
Victor Aquiles <victoraqalencar@gmail.com>
2021-01-07 10:47:08 -0300

    Lor regions and endpoints

--- riotwrapper/const/lor_const.py
+++ riotwrapper/const/lor_const.py
@@ -2,3 +2,15 @@
 # fmt: off
 
 """List of Legends of Runeterra API available regions and endpoints."""
+
+REGION_URL = {
+    "AMERICAS": "https://americas.api.riotgames.com",
+    "ASIA": "https://asia.api.riotgames.com",
+    "EUROPE": "https://europe.api.riotgames.com",
+}
+
+API_PATH = {
+    "match_ids": "{region_url}/lor/match/v1/matches/by-puuid/{puuid}/ids",
+    "match_by_id": "{region_url}/lor/match/v1/matches/{match_id}",
+    "leaderboard": "{region_url}/lor/ranked/v1/leaderboards",
+}
